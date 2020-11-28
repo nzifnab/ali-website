7c61b9983408a8abd89bc2d092a31c1861199e25
Nathan Benes <nathan.benes@gmail.com>
2020-11-28 16:54:24 -0700

    Buttons a little funky atm

--- app/javascript/ali_website/index.js
+++ app/javascript/ali_website/index.js
@@ -1,6 +1,6 @@
 (function($){
   $(function(){
-    $(".js-sortable-table").DataTable({
+    var table = $(".js-order-table").DataTable({
       paging: false,
       columnDefs: [
         {
@@ -23,6 +23,19 @@
           }
         }
       ]
+    });
+
+    $(".js-order-filter-buttons button").on('click', function(e) {
+      e.preventDefault();
+      var currentVal = $(this).data("active");
+      var newVal = !currentVal;
+
+      $(".js-order-filter-buttons button").data("active", false);
+      $(this).data("active", newVal);
+
+      if(newVal){
+        table.column($(this).data("filter-column")).search($(this).data("filter-value"), true);
+      }
     });
   });
 })(jQuery);
