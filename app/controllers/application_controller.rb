54704ec5fe3665b828289793376661023075be57
Nathan Benes <nathan.benes@gmail.com>
2020-12-08 02:44:45 -0700

    Added easy way to turn off corp member flag for yourself; if corp_member param doesn't match the exact token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
   helper_method :corp_member?
   def corp_member?
-    corp_member_token == Rails.application.config.corp_member_token
+    !!corp_member_token
   end
 
   def corp_member_token
-    if params[:corp_member].present?
+    if params[:corp_member].present? && params[:corp_member] == Rails.application.config.corp_member_token
       session[:corp_token] = params[:corp_member]
+    elsif params[:corp_member].present?
+      session.delete(:corp_token)
     end
 
     session[:corp_token]
